5ab338facde39a7a765df4e339144f68e45ba685
Renato Said <renk1009@hotmail.com>
2023-04-28 00:53:03 -0300

    novo js menu

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const hamburguer = document.querySelector(".menu-hamburguer");
 const navMenu = document.querySelector(".nav-menu");
-const navItem = document.querySelector(".nav-item")
+  
 
 hamburguer.addEventListener("click", () => {
    hamburguer.classList.toggle("active");
    navMenu.classList.toggle("active");
-   navItem.classList.toggle("active"); 
+   
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("clock", () => {
+
+-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburguer.classList.remove("active");
     navMenu.classList.remove("active");
-    navItem.classList.toggle("active");
 }))
